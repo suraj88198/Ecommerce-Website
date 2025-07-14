f0eb7d4c5347225fce309487bb9faa101b5a1bee
Suraj <suraj.kushwah@think.tank>
2025-07-14 17:20:58 +0530

    conflict resolved

--- src/components/categorySection/CategorySection.tsx
+++ src/components/categorySection/CategorySection.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useState } from 'react';
 import Slider from 'react-slick';
 import { PhoneIcon, ComputerIcon, SmartWatchIcon, CameraIcon, HeadPhonesIcon, GamingIcon } from '@/utils/icons/icons';
